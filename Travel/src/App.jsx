19d132ea14822d9b9a23b0524a23cb41ba56d4cd
himanshu6404 <156365855+himanshu6404@users.noreply.github.com>
2025-05-13 11:53:01 +0530

    correcting spelling of home.jsx

--- Travel/src/App.jsx
+++ Travel/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./home.jsx";
+import Home from "./Home.jsx";
 import SignIn from "./SignIn.jsx";
 import Dashboard from "./Dashboard.jsx";
 import SignUp from "./SignUp.jsx";
